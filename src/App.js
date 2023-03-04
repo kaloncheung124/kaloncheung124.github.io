96cc50efae31338afc24556c58486bf28b200a5b
Kalon Cheung <kaloncheung124@gmail.com>
2023-03-04 15:15:04 -0800

    minor cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 
 function App() {
-    const something = 3;
-
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Kalon Cheung</h1>
-                <p>Website in progress</p>
             </header>
         </div>
     );
