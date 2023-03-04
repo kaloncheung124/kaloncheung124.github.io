f383a1dc225fffcb5ce120469089cb2ad4ed33bc
Kalon Cheung <kaloncheung124@gmail.com>
2023-03-04 12:37:41 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    const something = 3;
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Kalon Cheung</h1>
+                <p>Website in progress</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
