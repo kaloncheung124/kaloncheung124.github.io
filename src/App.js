a07279e3e9af4bea0945ed1223a0558ddf016a17
Kalon Cheung <kaloncheung124@gmail.com>
2023-08-06 18:46:02 -0700

    replacing favicon

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Kalon Cheung</h1>
+                <h1>
+                    <a
+                        className="App-link"
+                        href="https://www.linkedin.com/in/kalon-cheung-b7452486/"
+                    >
+                        Kalon Cheung
+                    </a>
+                </h1>
             </header>
         </div>
     );
