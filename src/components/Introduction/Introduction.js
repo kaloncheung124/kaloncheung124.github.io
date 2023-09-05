de07fa6941f3bcfee98fb3c1a0dfa16598059a3b
Kalon Cheung <kaloncheung124@gmail.com>
2023-09-04 17:49:25 -0700

    adding clickable descriptor

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,8 +1,25 @@
-export const Introduction = () => (
-    <div className="App-intro">
-        <div className="App-name">Hi, I'm Kalon Cheung.</div>
-        <div className="App-subtitle">
-            <p>I build beautiful things for the web.</p>
+import { useState, useCallback } from 'react';
+
+const descriptors = ['beautiful', 'imaginative', 'useful'];
+
+export const Introduction = () => {
+    const [descriptorIndex, setDescriptorIndex] = useState(0);
+    const descriptor = descriptors[descriptorIndex];
+
+    const handleChangeDescriptor = useCallback(() => {
+        setDescriptorIndex(currentDescriptorIndex => (currentDescriptorIndex + 1) % descriptors.length);
+    }, [setDescriptorIndex]);
+
+    return (
+        <div className="App-intro">
+            <div className="App-name">Hi, I'm Kalon Cheung.</div>
+            <div className="App-subtitle">
+                <p>
+                    I build 
+                    <span className="description-highlight" onClick={handleChangeDescriptor}> {descriptor} </span>
+                    things for the web.
+                </p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
